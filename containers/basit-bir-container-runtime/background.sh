801b088ede87bc8f0b6d1db19a7e8fde8abc269c
Fatih Boy <fatih@enterprisecoding.com>
2019-10-29 22:33:24 +0300

    asset indirme eklendi

---
+++ containers/basit-bir-container-runtime/background.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/fatihboy/katacoda-scenarios/master/containers/basit-bir-container-runtime/assets/busybox.tar
+echo "done" >> /root/katacoda-finished
+echo "done" >> /root/katacoda-background-finished
