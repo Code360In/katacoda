bbe761b7e68d83e46af53ba701e7d9150e15665e
Fatih Boy <fatih@enterprisecoding.com>
2019-11-20 11:26:15 +0300

    docker registery ve metric server senaryolarındaki helm kurulumları güncellendi.

--- kubernetes/metrics-server-kurulumu/env-init.sh
+++ kubernetes/metrics-server-kurulumu/env-init.sh
@@ -1,6 +1,6 @@
 kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-export HELM_RELEASE=$(curl -s https://api.github.com/repos/helm/helm/releases/latest | grep tag_name | cut -d '"' -f 4)
+export HELM_RELEASE=$(curl --silent "https://api.github.com/repos/helm/helm/releases" | grep -Po '"tag_name": "\Kv2\..*?(?=")' | head -1)
 curl https://get.helm.sh/helm-$HELM_RELEASE-linux-amd64.tar.gz -o helm-$HELM_RELEASE-linux-amd64.tar.gz
 tar -zxvf helm-$HELM_RELEASE-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
