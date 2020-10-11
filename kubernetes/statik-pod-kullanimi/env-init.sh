86305a4220e01c54945ca02dd2825811bb58fb91
Fatih Boy <fatih@enterprisecoding.com>
2020-10-11 15:07:39 +0300

    statik pod typo düzeltmesi

--- kubernetes/statik-pod-kullanimi/env-init.sh
+++ kubernetes/statik-pod-kullanimi/env-init.sh
@@ -3,5 +3,5 @@ echo "source <(kubectl completion bash)" >> ~/.bashrc
 alias k=kubectl
 complete -F __start_kubectl k
 
-ssh node01 mkdir -p /root/.kubect
-scp /root/.kubect/config root@node01:/root/.kubect/config
+ssh node01 mkdir -p /root/.kubectl
+scp /root/.kubectl/config root@node01:/root/.kubectl/config
