9580e8800ffe2dd1118e1a0da5aef570e43e921a
Fatih Boy <fatih@enterprisecoding.com>
2020-10-11 15:08:35 +0300

    statik pod init script fix

--- kubernetes/statik-pod-kullanimi/env-init.sh
+++ kubernetes/statik-pod-kullanimi/env-init.sh
@@ -3,5 +3,5 @@ echo "source <(kubectl completion bash)" >> ~/.bashrc
 alias k=kubectl
 complete -F __start_kubectl k
 
-ssh node01 mkdir -p /root/.kubectl
-scp /root/.kubectl/config root@node01:/root/.kubectl/config
+ssh node01 mkdir -p /root/.kube
+scp /root/.kube/config root@node01:/root/.kube/config
