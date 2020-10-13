118c5f0bed7cd517af06d83a9c00326ef4c60151
Fatih Boy <fatih@enterprisecoding.com>
2020-10-13 16:52:10 +0300

    hpa ilklendirmesi güncellendi

--- kubernetes/hpa-kullanimi/assets/ilklendir.sh
+++ kubernetes/hpa-kullanimi/assets/ilklendir.sh
@@ -26,7 +26,7 @@ done
 echo ""
 echo "Metik server hazırlanıyor"
 METRICS_SERVER_RELEASE=$(curl --silent "https://api.github.com/repos/kubernetes-sigs/metrics-server/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-curl -L https://github.com/kubernetes-sigs/metrics-server/releases/download/${METRICS_SERVER_RELEASE}/components.yaml 2>/dev/null &> /dev/null | sed s/'args:'/'args:\n          - --kubelet-insecure-tls'/ > /tmp/components.yaml
+curl -L https://github.com/kubernetes-sigs/metrics-server/releases/download/${METRICS_SERVER_RELEASE}/components.yaml 2>/dev/null &> /dev/null | sed s/'args:'/'args:\n          - --kubelet-insecure-tls\n          - --kubelet-preferred-address-types=InternalIP'/ > /tmp/components.yaml
 kubectl apply -f /tmp/components.yaml 2>/dev/null &> /dev/null
 
 curl -L https://raw.githubusercontent.com/enterprisecoding-ltd/k8s-ornekleri/master/hpa/ornek-uygulama.yaml -o ornek-uygulama.yaml 2>/dev/null &> /dev/null
