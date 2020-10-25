fc7fb1fe055307a34b75d2ce4000d5c01b7bfd78
Fatih Boy <fatih@enterprisecoding.com>
2020-10-25 16:00:52 +0300

    RKE kurulumuna 2. node için timeout eklendi

--- rancher/rke-kurulumu/env-init.sh
+++ rancher/rke-kurulumu/env-init.sh
@@ -6,6 +6,14 @@ rm -f /root/.ssh/id_rsa
 
 if [ $HOSTNAME == "controlplane" ]; then
     MASTER_IP=$(hostname -I | cut -d' ' -f1)
+
+    EXIT_STATUS=1
+    while [ EXIT_STATUS -ne 0 ]
+    do
+        timeout 60 bash -c "</dev/tcp/node01/22"
+        EXIT_STATUS=$?
+    done
+    
     NODE01_IP=$(ssh node01 hostname -I | cut -d' ' -f1)
 
     cat <<EOF >> /root/rancher-cluster.yml
