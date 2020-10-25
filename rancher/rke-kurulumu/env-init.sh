f4478006543c7f0f1a83829347ed7f3fee64c3a4
Fatih Boy <fatih@enterprisecoding.com>
2020-10-25 16:03:21 +0300

    RKE script iyileştirmesi

--- rancher/rke-kurulumu/env-init.sh
+++ rancher/rke-kurulumu/env-init.sh
@@ -1,19 +1,8 @@
 systemctl stop kubelet
 systemctl disable kubelet
 
-rm -f /usr/bin/kubectl
-rm -f /root/.ssh/id_rsa
-
 if [ $HOSTNAME == "controlplane" ]; then
-    MASTER_IP=$(hostname -I | cut -d' ' -f1)
-
-    EXIT_STATUS=1
-    while [ EXIT_STATUS -ne 0 ]
-    do
-        timeout 60 bash -c "</dev/tcp/node01/22"
-        EXIT_STATUS=$?
-    done
-    
+    MASTER_IP=$(hostname -I | cut -d' ' -f1) 
     NODE01_IP=$(ssh node01 hostname -I | cut -d' ' -f1)
 
     cat <<EOF >> /root/rancher-cluster.yml
@@ -26,3 +15,6 @@ nodes:
   role: ['worker']
 EOF
 fi
+
+rm -f /usr/bin/kubectl
+rm -f /root/.ssh/id_rsa
