ed47da3ead180512e3e4bd20a67ba1356f37af90
Fatih Boy <fatih@enterprisecoding.com>
2020-10-26 19:26:09 +0300

    Rancher yedekleme senaryosu eklendi

--- rancher/rancher-yedekleme/assets/ilklendir.sh
+++ rancher/rancher-yedekleme/assets/ilklendir.sh
@@ -65,4 +65,24 @@ EOF
 
    echo ""
    echo "RKE Cluster'ı kullanıma hazır..."
+
+   echo "Gereksinimler hazırlanıyor..."
+   curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+   kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
+   kubectl create namespace cert-manager
+
+   helm repo add jetstack https://charts.jetstack.io
+   helm repo update
+
+   helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.15.0
+   kubectl wait --for condition=available deploy cert-manager -n cert-manager
+
+   echo "Rancher hazırlanıyor..."
+   kubectl create namespace cattle-system
+   helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+   helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=[[HOST_SUBDOMAIN]]-443-[[KATACODA_HOST]].environments.katacoda.com
+   kubectl wait --for condition=available -n cattle-system deploy/rancher
+
+   while true; do curl -sLk https://127.0.0.1/ping && break; printf "."; sleep 2; done
 fi
---
+++ rancher/rancher-yedekleme/step1.md
@@ -0,0 +1,13 @@
+# Lab Ortamı
+
+Sizin için 2 node üzerinde bir RKE Cluster'ı kurulu şekildedir. Sağ bölümde yer alan **Terminal** segmesinde bu control plane node'una ait Terminal kullanılabilir durumdadır. Bu terminal üzerinden aşağıda belirtilen adımlarda detayı verilen senaryoyu deneyimleyebilirsiniz.
+
+## RKE Cluster Yedekleme
+
+Rancher Kuberneter Engine Cluster'ının yedeği rke uygulaması yardımıyla elle alınabilir. Bunun için aşağıdaki komutu çalıştırın;
+
+`rke etcd snapshot-save --config rancher-cluster.yml --name ilk-yedek`{{execute}}
+
+Bu komut **ilk-yedek** adıyla bir yedek alacaktır. İşlemin tamamlanması ardından aşağıdaki komutla yedek dizininde **ilk-yedek.zip** adıyla yedek dosyasının oluştuğunu teyit edin;
+
+`ls -al /opt/rke/etcd-snapshots/`{{execute}}
