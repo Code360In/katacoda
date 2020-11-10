761a03ba29ea321691a231ebe273e87bf50e9d96
Fatih Boy <fatih@enterprisecoding.com>
2020-11-10 18:11:31 +0300

    Jenkins ilk iş senaryosu güncellendi

--- jenkins/ilk-is/env-init.sh
+++ jenkins/ilk-is/env-init.sh
@@ -1,22 +1,14 @@
-systemctl stop kubelet
-systemctl disable kubelet
+ADMIN_PASS=$(openssl rand -base64 12)
+NODE_IP=$(hostname -I | cut -d' ' -f1)
 
-rm -f /usr/bin/kubectl
+echo $ADMIN_PASS > /var/jenkins/secrets/jenkins-pass
 
-if [ $HOSTNAME == "controlplane" ]; then
-    ADMIN_PASS=$(openssl rand -base64 12)
+useradd -m -d /opt/jenkins/ jenkins
+echo "jenkins:jenkins" | sudo chpasswd
 
-    echo $ADMIN_PASS > /var/jenkins/secrets/jenkins-pass
-
-    NODE01_IP=$(getent hosts node01 | awk '{ print $1 }')
-
-    docker run --name jenkins \
+docker run --name jenkins \
         -d -p 8080:8080 -p 50000:50000  \
         -v /var/jenkins/secrets:/var/secrets \
         -v /var/jenkins/init.groovy.d:/usr/share/jenkins/ref/init.groovy.d \
-        --add-host node01:$NODE01_IP \
+        --add-host node01:$NODE_IP \
         quay.io/enterprisecoding/jenkins-suggested:latest
-else
-    useradd -m -d /opt/jenkins/ jenkins
-    echo "jenkins:jenkins" | sudo chpasswd
-fi
--- jenkins/ilk-is/step1.md
+++ jenkins/ilk-is/step1.md
@@ -1,11 +1,9 @@
 # Ortam
 
-Bu senaryo için bir Jenkins sunucusu ve bir ssh agent kurulum ve yapılandırması sizin için yapılmıştır. Sağ bölümde yer alan terminallerden bu sunucularda işlem yapabilirsiniz.
+Bu senaryo için bir Jenkins ve ssh agent kurulum ve yapılandırması sizin için yapılmıştır. Sağ bölümde yer alan terminalden agent sunucusunda işlem yapabilirsiniz.
 
-Jenkins arayüzüne aşağıdaki adresten ulaşabilirsiniz;
+Jenkins arayüzüne sağ taraftaki **Jenkins UI** segmesinden ulaşabilirsiniz.
 
-https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com
-
-Giriş için ihtiyacınız olan kullanıcı adı ve şifresi **Terminal Host 1**'de sizlere verilmiştir.
+Giriş için ihtiyacınız olan kullanıcı adı ve şifresi **Terminal**'de sizlere verilmiştir.
 
 **Continue** butonuna basarak sıradaki adıma geçebilirsiniz.
